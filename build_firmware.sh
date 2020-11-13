a8042b49423741f3f66ec496ccc18887d96616b4
SPARCie <sparcie@notgonnatell.nl>
2020-11-13 20:34:33 +0100

    2.4

--- build_firmware.sh
+++ build_firmware.sh
@@ -8,6 +8,6 @@ export GCC5_AARCH64_PREFIX=aarch64-linux-gnu-
 export WORKSPACE=$PWD
 export PACKAGES_PATH=$WORKSPACE/edk2:$WORKSPACE/edk2-platforms:$WORKSPACE/edk2-non-osi
 source edk2/edksetup.sh || exit 1
-cp bl31.bin edk-non-iso/Platform/RaspberryPi/RPi4/TrustedFirmware/bl31.bin
+cp bl31.bin edk2-non-osi/Platform/RaspberryPi/RPi4/TrustedFirmware/bl31.bin
 build -a AARCH64 -t GCC5 -p edk2-platforms/Platform/RaspberryPi/RPi4/RPi4.dsc -b DEBUG --pcd gEfiMdeModulePkgTokenSpaceGuid.PcdFirmwareVendor=L"https://github.com/pftf/RPi4" --pcd gEfiMdeModulePkgTokenSpaceGuid.PcdFirmwareVersionString=L"UEFI Firmware $APPVEYOR_REPO_TAG_NAME (DEBUG)" || exit 1
 build -a AARCH64 -t GCC5 -p edk2-platforms/Platform/RaspberryPi/RPi4/RPi4.dsc -b RELEASE --pcd gEfiMdeModulePkgTokenSpaceGuid.PcdFirmwareVendor=L"https://github.com/pftf/RPi4" --pcd gEfiMdeModulePkgTokenSpaceGuid.PcdFirmwareVersionString=L"UEFI Firmware $APPVEYOR_REPO_TAG_NAME" -D SECURE_BOOT_ENABLE=TRUE -D INCLUDE_TFTP_COMMAND=TRUE || exit 1
